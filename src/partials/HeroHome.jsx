7abf8d449bc8bf6deda04edbbeee9ff103212cd8
Daniela R <daniela.ramon12@gmail.com>
2023-03-21 20:10:39 -0600

    ✨ update footer

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -38,17 +38,12 @@ function HeroHome() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <img src={XWizeliners} alt="xwizeliners logo" data-aos="fade-up" className="mb-4 w-96 mx-auto" />
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">
-              Connect, share & grow together
+              We're a group of Wizeline alumni who have come together to connect, share & grow together.
             </p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
                 <a className="btn text-white bg-sky-600 hover:bg-sky-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">
-                  Primary action
-                </a>
-              </div>
-              <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">
-                  Secondary action
+                  Join us!
                 </a>
               </div>
             </div>
