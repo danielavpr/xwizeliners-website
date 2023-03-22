ea134fe25456d733a24bf56691f158b2182e0a74
Daniela R <daniela.ramon12@gmail.com>
2023-03-22 01:28:06 -0600

    💄 add animation

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -8,14 +8,14 @@ function FeaturesBlocks() {
         <div className="py-12 md:py-20">
 
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20" data-aos="fade-up">
             <h2 className="h2 mb-4">We are owners</h2>
             <p className="text-xl text-gray-400">
               We use ownership to create a better world for ourselves and our communities.
             </p>
           </div>
 
-          <img  src={values} alt="our values" />
+          <img  src={values} alt="our values" data-aos="fade-up" data-aos-delay="200" />
 
         </div>
       </div>
