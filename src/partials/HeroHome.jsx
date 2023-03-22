2f40fcb0cb9a5c4105edeb878a14cfabb4fa8502
Daniela R <daniela.ramon12@gmail.com>
2023-03-22 01:35:33 -0600

    ✨ add hero image

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Modal from '../utils/Modal';
-import HeroImage from '../images/hero-image-01.jpg';
+import Queretaro from '../images/queretaro.jpg';
 import XWizeliners from '../images/xwizeliners.png';
 
 function HeroHome() {
@@ -50,45 +50,8 @@ function HeroHome() {
           </div>
 
           {/* Hero image */}
-          <div>
-            <div className="relative flex justify-center items-center" data-aos="fade-up" data-aos-delay="200">
-              <img className="mx-auto" src={HeroImage} width="1024" height="504" alt="Hero" />
-              <a
-                className="absolute group"
-                href="#0"
-                onClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  setVideoModalOpen(true);
-                }}
-                aria-controls="modal"
-              >
-                <svg
-                  className="w-16 h-16 sm:w-20 sm:h-20 hover:opacity-75 transition duration-150 ease-in-out"
-                  viewBox="0 0 88 88"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <defs>
-                    <linearGradient x1="78.169%" y1="9.507%" x2="24.434%" y2="90.469%" id="a">
-                      <stop stopColor="#EBF1F5" stopOpacity=".8" offset="0%" />
-                      <stop stopColor="#EBF1F5" offset="100%" />
-                    </linearGradient>
-                  </defs>
-                  <circle fill="url(#a)" cx="44" cy="44" r="44" />
-                  <path
-                    className="fill-current text-sky-600"
-                    d="M52 44a.999.999 0 00-.427-.82l-10-7A1 1 0 0040 37V51a.999.999 0 001.573.82l10-7A.995.995 0 0052 44V44c0 .001 0 .001 0 0z"
-                  />
-                </svg>
-              </a>
-            </div>
-
-            {/* Modal */}
-            <Modal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
-              <div className="relative pb-9/16">
-                <iframe className="absolute w-full h-full" src="https://player.vimeo.com/video/174002812" title="Video" allowFullScreen></iframe>
-              </div>
-            </Modal>
+          <div className="relative flex justify-center items-center" data-aos="fade-up" data-aos-delay="200">
+            <img className="mx-auto" src={Queretaro} width="1024" height="504" alt="Hero" />
           </div>
         </div>
       </div>
