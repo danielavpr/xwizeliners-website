8a58d80ef86e08657a0bde039d5cb5d3dfcb4bc1
Daniela R <daniela.ramon12@gmail.com>
2023-04-01 23:52:55 -0600

    ✨ update photos

--- src/partials/Carrousel.jsx
+++ src/partials/Carrousel.jsx
@@ -3,24 +3,28 @@ import ArrowLeft from '../images/arrow-left.svg';
 import ArrowRight from '../images/arrow-right.svg';
 import Dot from '../images/dot-full.svg';
 import DotEmpty from '../images/dot-empty.svg';
+import Guadalajara from '../images/guadalajara.png';
+import Guadalajara2 from '../images/guadalajara2.png';
+import Guadalajara3 from '../images/guadalajara3.png';
+import Queretaro from '../images/queretaro.png';
+import Queretaro2 from '../images/queretaro2.png';
 
 function Carrousel() {
   const slides = [
     {
-      url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+      url: Guadalajara,
     },
     {
-      url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+      url: Queretaro,
     },
     {
-      url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
-    },
-
-    {
-      url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
+      url: Guadalajara2,
     },
     {
-      url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+      url: Queretaro2,
+    },
+    {
+      url: Guadalajara3,
     },
   ];
 
